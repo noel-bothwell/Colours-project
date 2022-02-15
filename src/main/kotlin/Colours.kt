f13f7cfc66ac5af3013edffdf7d3cf3e75219dcc
noel bothwell <20094424@mail.wit.ie>
2022-02-15 00:50:12 +0000

    returns the colours list

--- src/main/kotlin/Colours.kt
+++ src/main/kotlin/Colours.kt
@@ -4,7 +4,7 @@ class Colours {
 
     fun getAll (): List<String>{
         //TODO return the colours list
-        return listOf("this should return the colours list")
+        return listOf("Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet", "Black", "Silver")
     }
 
     fun numberOfColours (): Int{
@@ -34,23 +34,25 @@ class Colours {
 
     fun coloursStartingWithLetter(letter: Char) : List<String>{
         //TODO return the colours starting with the letter passed as a parameter
-        return listOf("this should return colours that start with ${letter}")
+        return listOf("this should return colours that start with $letter")
     }
 
     fun coloursWithSpecificNumberOfChars(size: Int) : List<String>{
         //TODO return the colours that have exactly the same number of chars as the number passed as a parameter
-        return listOf("this should return colours that have ${size} chars")
+        return listOf("this should return colours that have $size chars")
     }
 
     fun coloursWithLessCharsThan(size: Int) : List<String>{
         //TODO return the colours that have less chars than the number passed as s parameter
-        return listOf("this should return colours less than ${size} chars")
+        return listOf("this should return colours less than $size chars")
     }
 
-    fun isColourInTheList(colour : String) : Boolean{
+    fun isColourInTheList(colours : String) : Boolean{
         //TODO return true of the colour passed as a parameter is in the list and false otherwise
         // Note: the search should be case insensitive - if blue is passed as a parameter it should find Blue, BLUE etc.
-        return false
+        if (colours.contains(("Blue")))
+        return true
+        else return false
     }
 
     //TODO Write THREE additional functions of your choice that will perform some sort of
