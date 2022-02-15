b5f2c348fdb4a06b5b36bb5207b725a236cb0c52
noel bothwell <20094424@mail.wit.ie>
2022-02-15 00:58:34 +0000

    returns the size of the colours list

--- src/main/kotlin/Colours.kt
+++ src/main/kotlin/Colours.kt
@@ -9,7 +9,7 @@ class Colours {
 
     fun numberOfColours (): Int{
         //TODO return the size of the colours list
-        return 0
+        return colours.size
     }
 
     fun firstColour() : String{
